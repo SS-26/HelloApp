cb4f384d382fb68e781b8bfee9527c7e2707e610
SS-26 <shrusub26@gmail.com>
2026-04-05 21:10:57 +0530

    Update HelloApp1.java

--- HelloApp1.java
+++ HelloApp1.java
@@ -1,13 +1,27 @@
-public class HelloApp {
+public class UC6 {
+        public static void main(String[] args) {
 
-    public static void main(String[] args) {
+            String greeting;
 
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
+            if (args.length == 0) {
+                greeting = "Hello, World!";
+            } else {
+                StringBuilder nameBuilder = new StringBuilder();
+
+                // Enhanced for loop to iterate through arguments
+                for (String name : args) {
+                    nameBuilder.append(name).append(", ");
+                }
+
+                // Remove trailing ", "
+                if (nameBuilder.length() > 0) {
+                    String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+                    greeting = "Hello, " + names + "!";
+                } else {
+                    greeting = "Hello, World!";
+                }
+            }
+
+            System.out.println(greeting);
         }
-
     }
-}
